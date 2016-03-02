032880b17776b83f17e86b10f95cf8b9071c0aec
lucasviola <lucasmatzenbacher@gmail.com>
2016-03-02 11:51:35 -0300

    Tries to fix

--- client/templates/index.js
+++ client/templates/index.js
@@ -1,11 +1,11 @@
-Template.body.helpers({
+Template.challenges.helpers({
   challenges: function () {
     return Challenges.find({});
-  },
-  schedules: function () {
-    return Schedules.find({});
-  },
-  activities: function () {
-    return Activities.find({});
   }
+  // schedules: function () {
+  //   return Schedules.find({});
+  // },
+  // activities: function () {
+  //   return Activities.find({});
+  // }
 });
