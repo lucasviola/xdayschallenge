ae67ebb83094f3b6a7222340f8c9452408ecaeb8
lucasviola <lucasmatzenbacher@gmail.com>
2016-03-02 11:06:12 -0300

    Uncomments helpers

--- client/templates/index.js
+++ client/templates/index.js
@@ -1,13 +1,11 @@
-// Index helpers
-
-// Template.body.helpers({
-//   challenges: function () {
-//     return Challenges.find({});
-//   },
-//   schedules: function () {
-//     return Schedules.find({});
-//   },
-//   activities: function () {
-//     return Activities.find({});
-//   }
-// });
+Template.body.helpers({
+  challenges: function () {
+    return Challenges.find({});
+  },
+  schedules: function () {
+    return Schedules.find({});
+  },
+  activities: function () {
+    return Activities.find({});
+  }
+});
